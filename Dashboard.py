b5d8ef7816e6321248c0dd579096677309fb1f6d
Lucas Ordonio <lucasordonio_55@hotmail.com>
2024-04-25 09:08:01 -0300

    Salvando dash vendedores

---
+++ Dashboard.py
@@ -0,0 +1,127 @@
+import streamlit as st
+import pandas as pd
+import requests
+import plotly.express as px
+
+st.set_page_config(layout = 'wide')
+
+def formata_numero(valor,prefixo= ''):
+    for unidade in ['','mil']:
+        if valor <1000:
+            return f'{prefixo}{valor:.2f}{unidade}'
+        valor /=1000
+    return f'{prefixo}{valor:.2f} milhões'
+
+st.title('DASHBOARD DE VENDAS :shopping_trolley:')
+
+url = 'https://labdados.com/produtos'
+response = requests.get(url)
+dados = pd.DataFrame.from_dict(response.json())
+dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format = '%d/%m/%Y')
+
+## Construindo tabelas auxiliares
+### Tabelas Receita
+receita_estado = dados.groupby('Local da compra')[['Preço']].sum()
+receita_estado = dados.drop_duplicates(subset='Local da compra')[['Local da compra','lat','lon']].merge(receita_estado,left_on='Local da compra',right_index=True).sort_values('Preço',ascending=False)
+receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].sum().reset_index()
+receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
+receita_mensal['Mes'] = receita_mensal['Data da Compra'].dt.month_name()
+receita_categorias = dados.groupby('Categoria do Produto')[['Preço']].sum().sort_values('Preço',ascending=False)
+
+### Tabela Vendas
+
+
+###Tabelas Vendedores 
+vendedores = pd.DataFrame(dados.groupby('Vendedor')['Preço'].agg(['sum', 'count']))
+
+## Gráficos
+
+fig_mapa_receita = px.scatter_geo(receita_estado,
+                                  lat='lat',
+                                  lon='lon',
+                                  scope='south america',
+                                  size='Preço',
+                                  template= 'seaborn',
+                                  hover_name='Local da compra',
+                                  hover_data={'lat':False,'lon':False},
+                                  title= 'Receita por Estado')
+
+
+fig_receita_mensal = px.line(receita_mensal,
+                                                        x = 'Mes',
+                                                        y = 'Preço',
+                                                        markers = True,
+                                                        range_y = (0, receita_mensal.max()),
+                                                        color='Ano',
+                                                        line_dash = 'Ano',
+                                                        title = 'Receita mensal')
+
+fig_receita_mensal.update_layout(yaxis_title = 'Receita')
+
+
+fig_receita_estado = px.bar(receita_estado.head(),
+                            x = 'Local da compra',
+                            y = 'Preço',
+                            text_auto= True,
+                            title = 'Top estados (receita)')
+
+fig_receita_estado.update_layout(yaxis_title = 'Receita')
+
+fig_receita_categorias = px.bar(receita_categorias.head(),
+                            text_auto= True,
+                            title = 'Receita por categoria')
+
+## Visualização no Streamlit
+
+## Definindo a quantidade de abas / páginas a serem exibidas no painel do streamlit
+
+aba1, aba2, aba3 = st.tabs(['Receita','Quantidade de Vendas', 'Vendedores'])
+
+with aba1:
+    coluna1,coluna2 = st.columns(2)
+    with coluna1:
+        st.metric('Receita',formata_numero(dados['Preço'].sum().round(0),'R$'))
+        st.plotly_chart(fig_mapa_receita,use_container_width = True)
+        st.plotly_chart(fig_receita_estado,use_container_width=True)
+    with coluna2:
+        st.metric('Quantidade de vendas',formata_numero(dados.shape[0]))
+        st.plotly_chart(fig_receita_mensal,use_container_width = True)
+        st.plotly_chart(fig_receita_categorias,use_container_width=True)
+
+with aba2:
+    coluna1,coluna2 = st.columns(2)
+    with coluna1:
+        st.metric('Receita',formata_numero(dados['Preço'].sum().round(0),'R$'))
+       
+    with coluna2:
+        st.metric('Quantidade de vendas',formata_numero(dados.shape[0]))
+       
+with aba3:
+    coluna1,coluna2 = st.columns(2)
+    qtd_vendedores = st.number_input('Quantidade de vendedores', 2, 10, 5)
+    with coluna1:
+        st.metric('Receita',formata_numero(dados['Preço'].sum().round(0),'R$'))
+        fig_receita_vendedores = px.bar(
+        vendedores[['sum']].sort_values('sum', ascending=False).head(qtd_vendedores),
+        x='sum',
+        y=vendedores[['sum']].sort_values(['sum'], ascending=False).head(qtd_vendedores).index,
+        text_auto=True,
+        title=f'Top {qtd_vendedores} vendedores (receita)'
+        )
+        st.plotly_chart(fig_receita_vendedores,use_container_width=True)
+
+    with coluna2:
+        st.metric('Quantidade de vendas',formata_numero(dados.shape[0]))
+        fig_venda_vendedores = px.bar(
+        vendedores[['count']].sort_values('count', ascending=False).head(qtd_vendedores),
+        x='count',
+        y=vendedores[['count']].sort_values(['count'], ascending=False).head(qtd_vendedores).index,
+        text_auto=True,
+        title=f'Top {qtd_vendedores} vendedores (Quantidade de vendas)'
+        )
+        st.plotly_chart(fig_venda_vendedores,use_container_width=True)
+    
+
+#código para exibir o df  no streamlit
+# st.dataframe(dados)
+
