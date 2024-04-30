e298e48fc169eb31fd214a2375843c1e44103c5b
Lucas Ordonio <lucasordonio_55@hotmail.com>
2024-04-29 21:18:19 -0300

    inclusao de filtros_part1

--- Dashboard.py
+++ Dashboard.py
@@ -15,9 +15,32 @@ def formata_numero(valor,prefixo= ''):
 st.title('DASHBOARD DE VENDAS :shopping_trolley:')
 
 url = 'https://labdados.com/produtos'
-response = requests.get(url)
+regioes = ['Brasil', 'Centro-Oeste', 'Nordeste', 'Norte', 'Sudeste', 'Sul']
+
+st.sidebar.title('Filtros')
+regiao = st.sidebar.selectbox('Região', regioes)
+
+if regiao == 'Brasil':
+    regiao = ''
+
+todos_anos = st.sidebar.checkbox('Dados de todo o período', value = True)
+
+if todos_anos:
+    ano = ''
+else:
+    ano = st.sidebar.slider('Ano', 2020, 2023)
+
+query_string = {'regiao':regiao.lower(), 'ano':ano}
+
+response = requests.get(url, params= query_string)
 dados = pd.DataFrame.from_dict(response.json())
 dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format = '%d/%m/%Y')
+
+
+filtro_vendedores = st.sidebar.multiselect('Vendedores', dados['Vendedor'].unique())
+
+if filtro_vendedores:
+    dados = dados[dados['Vendedor'].isin(filtro_vendedores)]
 
 ## Construindo tabelas auxiliares
 ### Tabelas Receita
@@ -30,11 +53,19 @@ receita_categorias = dados.groupby('Categoria do Produto')[['Preço']].sum().sor
 
 ### Tabela Vendas
 
+qtd_vendas_estado = dados.groupby('Local da compra')[['Local da compra']].count()
+qtd_vendas_estado = dados.drop_duplicates(subset='Local da compra')[['Local da compra','lat','lon']].merge(qtd_vendas_estado,left_on='Local da compra',right_index=True).sort_values('Local da compra',ascending=False)
+qtd_vendas_mensal  = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Local da compra'].count().reset_index()
+qtd_vendas_mensal['Ano'] = qtd_vendas_mensal['Data da Compra'].dt.year
+qtd_vendas_mensal['Mes'] = qtd_vendas_mensal['Data da Compra'].dt.month_name()
+qtd_vendas_mensal = qtd_vendas_mensal.rename(columns= {'Local da compra': 'Qtd de vendas'})
+qtd_vendas_categorias = dados['Categoria do Produto'].value_counts().reset_index()
+qtd_vendas_categorias.columns = ['Categoria do Produto', 'Quantidade de Vendas']
 
 ###Tabelas Vendedores 
 vendedores = pd.DataFrame(dados.groupby('Vendedor')['Preço'].agg(['sum', 'count']))
 
-## Gráficos
+## Gráficos receita Mensal
 
 fig_mapa_receita = px.scatter_geo(receita_estado,
                                   lat='lat',
@@ -71,6 +102,38 @@ fig_receita_categorias = px.bar(receita_categorias.head(),
                             text_auto= True,
                             title = 'Receita por categoria')
 
+## gráficos página quantidade de vendas
+fig_mapa_qtd_vendas = px.scatter_geo(qtd_vendas_estado,
+                                  lat='lat',
+                                  lon='lon',
+                                  scope='south america',
+                                  size='Local da compra_y',
+                                  template= 'seaborn',
+                                  hover_name='Local da compra',
+                                  hover_data={'lat':False,'lon':False},
+                                  title= 'Receita por Estado')
+
+fig_qtd_vendas_mensal = px.line(qtd_vendas_mensal,
+                                                        x = 'Mes',
+                                                        y = 'Qtd de vendas',
+                                                        markers = True,
+                                                        range_y = (0, qtd_vendas_mensal.max()),
+                                                        color='Ano',
+                                                        line_dash = 'Ano',
+                                                        title = 'Qtd de vendas mensal')
+
+fig_vendas_estado = px.bar(qtd_vendas_estado.head(),
+                            x = 'Local da compra',
+                            y = 'Local da compra_y',
+                            text_auto= True,
+                            title = 'Top estados (vendas)')
+
+fig_vendas_categorias = px.bar(qtd_vendas_categorias.head(),
+                            x= 'Categoria do Produto',
+                            y= 'Quantidade de Vendas',
+                            text_auto= True,
+                            title = 'Qtd vendas por categoria')
+
 ## Visualização no Streamlit
 
 ## Definindo a quantidade de abas / páginas a serem exibidas no painel do streamlit
@@ -86,19 +149,26 @@ with aba1:
     with coluna2:
         st.metric('Quantidade de vendas',formata_numero(dados.shape[0]))
         st.plotly_chart(fig_receita_mensal,use_container_width = True)
-        st.plotly_chart(fig_receita_categorias,use_container_width=True)
+        st.plotly_chart(fig_vendas_categorias,use_container_width=True)
 
 with aba2:
-    coluna1,coluna2 = st.columns(2)
+    coluna1,coluna2,coluna3 = st.columns([2, 2, 1])
     with coluna1:
         st.metric('Receita',formata_numero(dados['Preço'].sum().round(0),'R$'))
-       
+        st.plotly_chart(fig_mapa_qtd_vendas,use_container_width = True)
+        st.plotly_chart(fig_vendas_estado,use_container_width = True)
     with coluna2:
         st.metric('Quantidade de vendas',formata_numero(dados.shape[0]))
-       
+        st.plotly_chart(fig_qtd_vendas_mensal,use_container_width = True)
+        st.plotly_chart(fig_vendas_categorias,use_container_width = True)
+
+    with coluna3:
+        st.metric('Quantidade de estados',dados['Local da compra'].nunique())
+
+        
 with aba3:
-    coluna1,coluna2 = st.columns(2)
     qtd_vendedores = st.number_input('Quantidade de vendedores', 2, 10, 5)
+    coluna1,coluna2 = st.columns(2)
     with coluna1:
         st.metric('Receita',formata_numero(dados['Preço'].sum().round(0),'R$'))
         fig_receita_vendedores = px.bar(
