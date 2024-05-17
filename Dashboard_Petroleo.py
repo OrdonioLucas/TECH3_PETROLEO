087bd133078710c805365fcf7c1e9169b72cf914
Lucas Ordonio <lucasordonio_55@hotmail.com>
2024-05-17 06:52:45 -0300

    Atualização Página

--- Dashboard_Petroleo.py
+++ Dashboard_Petroleo.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import requests
 import plotly.express as px
 
 
@@ -19,6 +18,8 @@ df = df.set_index('Date')
 df['Day_of_week'] = df.index.dayofweek
 df['Month'] = df.index.month
 df['Year'] = df.index.year
+df['Year'] = df['Year'].astype(int)
+
 
 
 st.set_page_config(layout = 'wide')
@@ -37,25 +38,40 @@ st.sidebar.title('Filtros')
 
 
 
-ano = st.sidebar.slider('Ano', df['Year'].min(), df['Year'].max(),(df['Year'].min(),df['Year'].max()))
+ano = st.sidebar.slider('Ano', 
+    int(df['Year'].min()), 
+    int(df['Year'].max()),
+    (int(df['Year'].min()), int(df['Year'].max()))
+)
+
 df_filtrado = df[(df['Year'] >= ano[0]) & (df['Year'] <= ano[1])]
 
 query_string = {'ano':ano}
 
+# Criando variaveis dos cards
 
+media =  df_filtrado['Price'].mean().round(0)
+minimo = df_filtrado['Price'].min().round(0)
+maximo = df_filtrado['Price'].max().round(0)
+
+media_formatada = f'U${media:,.2f}'
+min_formatada = f'U${minimo:,.2f}'
+max_formatada = f'U${maximo:,.2f}'
 
 fig_preco_petroleo = px.line(df_filtrado, x=df_filtrado.index, y='Price', title='Preço do Petróleo por Ano', line_shape='linear')
 
 aba1, aba2, aba3 = st.tabs(['Preço Petróleo','Análises', 'Predição'])
 
-coluna1, coluna2, coluna3 = st.columns([4, 1, 1])
+coluna1, coluna2, coluna3 = st.columns([1, 1, 1])
 
 with coluna1:
-    st.metric('Preço Médio do Petróleo U$$', df_filtrado['Price'].mean().round(0))
-    st.plotly_chart(fig_preco_petroleo,use_container_width=True)
-
+    st.metric('Preço Médio do Petróleo U$$', media_formatada)
+    
 with coluna2:
-    st.metric('Preço Mínimo do Petróleo U$$', df_filtrado['Price'].min().round(0))
+    st.metric('Preço Mínimo do Petróleo U$$', min_formatada)
 
 with coluna3:
-    st.metric('Preço Máximo do Petróleo U$$', df_filtrado['Price'].max().round(0))
+    st.metric('Preço Máximo do Petróleo U$$', max_formatada)
+
+st.plotly_chart(fig_preco_petroleo,use_container_width=True)
+
